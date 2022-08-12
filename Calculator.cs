3e30722c3d0a5571161821b3966493e16f3e53da
Glyson <kauaglyson@gmail.com>
2022-08-12 20:30:17 -0300

    interactive menu
    using ENUM and SWITCH to make the menu functions

--- Calculator.cs
+++ Calculator.cs
@@ -1,17 +1,46 @@
 using System;
 namespace _CSharpCalculator
 {
+
     class Calculator : Formulas
     {
         //double num1, num2;
     }
 
+    // enum with options, so i can work with switch. This enum must start by number one
+    public enum Option{ Addition = 1, Subtraction, Multiplication, Division };
     static class Menu
     {
         public static void menu()
         {
             Console.WriteLine("Welcome to my calculator. Choose one of the options below: ");
             Console.WriteLine("1- Addition\n2- Subtraction\n3- Multiplication\n4- Division");
+            // recieving the user input (it reads as string) and converting to an integer type
+            int index = Convert.ToInt32(Console.ReadLine());
+            // the enum Option variable (chosenOption) convert the input (now an integer type) to the enum Option type, then recieve its value
+            Option chosenOption = (Option)index;
+            switch (chosenOption)
+            {
+                case Option.Addition:
+                Console.WriteLine($"You choose {chosenOption}");
+                break;
+                
+                case Option.Subtraction:
+                Console.WriteLine($"You choose {chosenOption}");
+                break;
+
+                case Option.Multiplication:
+                Console.WriteLine($"You choose {chosenOption}");
+                break;
+
+                case Option.Division:
+                Console.WriteLine($"You choose {chosenOption}");
+                break;
+
+                default:
+                Console.WriteLine("You choose an invalid option! Please try again");
+                break;
+            }
         }
     }
 
