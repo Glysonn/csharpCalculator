292552ae817d74950a0f7f624c2fcec0abbf45f1
Glyson <kauaglyson@gmail.com>
2022-08-12 20:02:47 -0300

    creating Menu class
    Menu is a static class to show the menu options

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,18 @@
+using System;
 namespace _CSharpCalculator
 {
     class Calculator : Formulas
     {
-        
+        //double num1, num2;
     }
+
+    static class Menu
+    {
+        public static void menu()
+        {
+            Console.WriteLine("Welcome to my calculator. Choose one of the options below: ");
+            Console.WriteLine("1- Addition\n2- Subtraction\n3- Multiplication\n4- Division");
+        }
+    }
+
 }
