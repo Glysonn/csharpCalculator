e7f7f4905d1280a259f2ee503e4c68d0e8b9d8b1
Glyson <kauaglyson@gmail.com>
2022-08-12 22:16:17 -0300

    Creating the Calc() method
    The method decides which math operator must be used. It also has new variables.

--- Calculator.cs
+++ Calculator.cs
@@ -1,12 +1,56 @@
 using System;
 namespace _CSharpCalculator
 {
-
     class Calculator : Formulas
     {
-        //double num1, num2;
+        double num1, num2, result;
+        public void Calc()
+        {
+            // new Option (enum) type variable. It also calls the menu() method
+            Option escolhida = Menu.menu();
+            // switch case to decide which operation must be done
+            switch (escolhida)
+            {
+                case (Option.Addition):
+                    Console.WriteLine($"You choose {escolhida}!\nPlease, enter your first nummber: ");
+                    this.num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"You first number is {this.num1}!\nNow choose your second number: ");
+                    this.num2 = Convert.ToDouble(Console.ReadLine());
+                    this.result = addition(this.num1, this.num2);
+                    Console.WriteLine($"The result of {this.num1}+{this.num2} is {this.result}");
+                    break;
+
+                case Option.Subtraction:
+                    Console.WriteLine($"You choose {escolhida}!\nPlease, enter your first nummber: ");
+                    this.num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"You first number is {this.num1}!\nNow choose your second number: ");
+                    this.num2 = Convert.ToDouble(Console.ReadLine());
+                    this.result = subtraction(this.num1, this.num2);
+                    Console.WriteLine($"The result of {this.num1}-{this.num2} is {this.result}");
+                    break;
+                
+                case Option.Multiplication:
+                    Console.WriteLine($"You choose {escolhida}!\nPlease, enter your first nummber: ");
+                    this.num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"You first number is {this.num1}!\nNow choose your second number: ");
+                    this.num2 = Convert.ToDouble(Console.ReadLine());
+                    this.result = multiplication(this.num1, this.num2);
+                    Console.WriteLine($"The result of {this.num1}*{this.num2} is {this.result}");
+                    break;
+                
+                case Option.Division:
+                    Console.WriteLine($"You choose {escolhida}!\nPlease, enter your first nummber: ");
+                    this.num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"You first number is {this.num1}!\nNow choose your second number: ");
+                    this.num2 = Convert.ToDouble(Console.ReadLine());
+                    this.result = division(this.num1, this.num2);
+                    Console.WriteLine($"The result of {this.num1}/{this.num2} is {this.result}");
+                    break;
+
+                default:
+                Console.WriteLine("The chosen option doesn't exists. Please try again!");
+                break;
+            }
+        }
     }
-
-
-
 }
